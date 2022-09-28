c990bef9655e72af729a8fa38928273e6e08f27e
Mudana-Grune <mudana@gruneasia.info>
2022-09-28 19:53:29 +0800

    Implement xml-parser from node-expat / sax package
    https://youtrack.weseek.co.jp/issue/GW-7891
    - Remove sax and node-expat  package
    - Add libxmljs package
    - Implement libxmljs to parse XML

--- packages/app/src/components/PageEditor/DrawioModal.jsx
+++ packages/app/src/components/PageEditor/DrawioModal.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import i18next from 'i18next';
 
+import i18next from 'i18next';
+import PropTypes from 'prop-types';
 import {
   Modal,
   ModalBody,
 } from 'reactstrap';
 
-import { withUnstatedContainers } from '../UnstatedUtils';
 import AppContainer from '~/client/services/AppContainer';
 import EditorContainer from '~/client/services/EditorContainer';
 import { getDiagramsNetLangCode } from '~/client/util/locale-utils';
+
+import { withUnstatedContainers } from '../UnstatedUtils';
 
 class DrawioModal extends React.PureComponent {
 
@@ -92,9 +93,12 @@ class DrawioModal extends React.PureComponent {
 
     if (typeof event.data === 'string' && event.data.match(/mxfile/)) {
       if (event.data.length > 0) {
-        const parser = new DOMParser();
-        const dom = parser.parseFromString(event.data, 'text/xml');
-        const value = dom.getElementsByTagName('diagram')[0].innerHTML;
+        const libxml = require('libxmljs');
+        const dom = libxml.parseXml(event.data);
+
+        // const dom = parser.parseFromString(event.data, 'text/xml');
+        // const value = dom.getElementsByTagName('diagram')[0].innerHTML;
+        const value = '';
 
         if (this.props.onSave != null) {
           this.props.onSave(value);
